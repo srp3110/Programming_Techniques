d85469162dfdd4553eea5e8849faea403bfdaefd
srp3110 <surrya20@gmail.com>
2023-09-28 21:50:50 +0800

    test 1 hour video

--- Tutorials/Solutions/test.c
+++ Tutorials/Solutions/test.c
@@ -1,56 +1,9 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
-    int grade;
-    unsigned int aCount = 0;
-    unsigned int bCount = 0;
-    unsigned int cCount = 0;
-    unsigned int dCount = 0;
-    unsigned int fCount = 0;
-
-    puts("Enter the letter grades." );
-    puts("Enter the EOF character to end input" );
-
-    while ((grade = getchar()) != EOF){
-        switch(grade){
-            case 'A':
-            case'a':
-                ++aCount;
-                break;
-            case 'B':
-            case'b':
-                ++bCount;
-                break;
-            case 'C':
-            case'c':
-                ++cCount;
-                break;
-            case 'D':
-            case'd':
-                ++dCount;
-                break;
-            case 'F':
-            case'f':
-                ++fCount;
-                break;
-            case '\n':
-            case '\t':
-            case ' ':
-                break;
-            
-            default:
-                printf("%s", "Incorrect letter grade entered." );
-                break;
-        }
-    }
-
-    puts("\nTotals for each letter grade are: ");
-    printf("A: %u\n", aCount);
-    printf("B: %u\n", bCount);
-    printf("C: %u\n", cCount);
-    printf("D: %u\n", dCount);
-    printf("F: %u\n", fCount);
+    printf("%f", 5-3);
 
     return 0;
 }
