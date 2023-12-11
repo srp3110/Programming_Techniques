bc96601457b03f81f016c7fbf0ff62110f916d3f
srp3110 <surrya20@gmail.com>
2023-12-12 05:32:30 +0800

    summary

--- Final Exam Practice/Tut-1.c
+++ Final Exam Practice/Tut-1.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 
-int main() {
-    int size=5, row, col;
+int main()
+{
+	int in, X[20], n1, n2;
 
-    for (row=1; row<=size; row++){
-        for (col=row; col<=size; col++){
-            // if(col==1||row==col||row==size){
-                printf("* ");
-            // } else{
-            //     printf("  ");
-            // }
-        }
-        printf("\n");
-    }
+	for (n1 = 0; n1 < 20; n1++)
+	{
+		printf("Enter a number between 10 - 100: ");
+		scanf("%d", &X[n1]);
 
+		if (n1 != 0)
+		{
+			for (n2 = 0; n2 < n1; n2++)
+			{
+				if (X[n1] == X[n2])
+				{
+					break;
+				}
+			}
+		}
+		else
+		{
+			printf("%d\n", X[n1]);
+		}
+
+		if (n2 == n1)
+		{
+			printf("%d\n", X[n1]);
+		}
+		
+	}
 	return 0;
-
 }
